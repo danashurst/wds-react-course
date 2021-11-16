38c1b28e00b96d9e75eaa1780eb9b4dfeee2ad72
Dan Ashurst <dan@danashurst.com>
2021-11-16 10:47:10 +0000

    add swagger static json endpoint

--- project-2.API/Program.cs
+++ project-2.API/Program.cs
@@ -11,6 +11,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
